fe7ffa2ffba33f15a19a9e19eab4a17b36e1db32
JoseTyler101 <JoseTyler101@gmail.com>
2019-05-16 09:55:50 -0400

    Built 1st Model

---
+++ controllers/squadController.js
@@ -0,0 +1,48 @@
+const Squad = require("../models/Squad");
+
+const squadController = {
+  // INDEX
+  index: function (req, res) {
+    Category.find().then(Cats => {
+      console.log(Cats)
+      res.render("cat/index", { Cats });
+    });
+  },
+
+  // NEW
+  new: function (req, res) {
+    res.render("cat/new");
+  },
+
+  // SHOW
+  show: function (req, res) {
+    Category.findById(req.params.id).populate("photographers").then(Cat => {
+      console.log(Cat)
+      res.render("cat/show", { Cat });
+    });
+  },
+
+  // CREATE
+  create: function (req, res) {
+    console.log(req);
+    Category.create(req.body).then(() => res.redirect("/"));
+  },
+
+  // UPDATE
+  //     Donut.findByIdAndUpdate(req.params.id, req.body, { new: true }).then(() => {
+  //       res.redirect("/" + req.params.id);
+  //     });
+  //   },
+
+  // DELETE
+  delete: function (req, res) {
+    Category.findByIdAndRemove(req.params.id).then(() => {
+      res.redirect("/");
+    });
+  }
+
+};
+
+
+// EXPORTS
+module.exports = squadController;
---
+++ models/Squad.js
@@ -0,0 +1,13 @@
+// requirements: require db/connection as 'mongoose'
+const mongoose = require('../db/connection')
+const Schema = mongoose.Schema
+
+const Squad = new Schema({
+    gamertag: String,
+    name: String,
+    age: String,
+    platform: String  
+})
+
+
+module.exports = mongoose.model('Squad', Squad)
