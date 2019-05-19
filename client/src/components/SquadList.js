ef85f81abf17d56c2405f1bc77daef99fb64e163
JoseTyler101 <JoseTyler101@gmail.com>
2019-05-18 21:21:57 -0400

    Squadlist is reading players

--- client/src/components/SquadList.js
+++ client/src/components/SquadList.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Squad from './Squad';
 
 class SquadList extends Component {
+    state = {
+        squadInfo:[]
+    }
+
+    componentDidMount(){
+        this.getSquads()
+    }
+
+    getSquads = () => {
+        axios.get('api/v1/squad').then((res)=>{
+            this.setState({squadInfo: res.data})
+        })
+    }
+
     render() {
         return (
             <div>
                 Im a squadlist
+                {this.state.squadInfo.map((spot, index )=>{
+
+return (
+   
+    <Squad key={index} 
+    gamertag={spot.gamertag}
+    name={spot.name}
+    _id={spot._id}
+    age={spot.age}
+    platform={spot.platform}
+    />
+)
+
+                })}
             </div>
         );
     }
