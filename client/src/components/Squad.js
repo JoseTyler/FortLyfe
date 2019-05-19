e6b1cc6b9ad5739812a5ac96ab44bc3957b863cd
JoseTyler101 <JoseTyler101@gmail.com>
2019-05-18 21:39:26 -0400

    Players showing up

--- client/src/components/Squad.js
+++ client/src/components/Squad.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Squad extends Component {
+
+    deletePlayer=()=>{
+        console.log(this.props._id)
+        axios.delete(`api/v1/squad/${this.props._id}`)
+    }
+
     render() {
         return (
             <div>
-                Im a squad
+                <p>{this.props.name}</p>
+                <p>{this.props.platform}</p>
+                <p>{this.props.age}</p>
+                <p>{this.props.gamertag} </p>
+                               
+        <button type="submit">Edit Player</button>
+        <button onClick={this.deletePlayer}>Delete Player</button>
             </div>
         );
     }
